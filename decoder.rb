6ec8b2e2e3f0ae9a7c275256f059e8ce2981c57e
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-07-13 20:19:05 +0100

    Fix rubocop errors

--- decoder.rb
+++ decoder.rb
@@ -1,80 +1,53 @@
-def decode_letter (input)
-  case input
-  when ".-"
-    output = "A"
-  when "-..."
-    output = "B"
-  when "-.-."
-    output = "C"
-  when "-.."
-    output = "D"
-  when "."
-    output = "E"
-  when "..-."
-    output = "F"
-  when "--."
-    output = "G"
-  when "...."
-    output = "H"    
-  when ".."
-    output = "I"
-  when ".---"
-    output = "J"
-  when "-.-"
-    output = "K"
-  when ".-.."
-    output = "L"
-  when "--"
-    output = "M"
-  when "-."
-    output = "N"
-  when "---"
-    output = "O"
-  when ".--."
-    output = "P"
-  when "--.-"
-    output = "Q"
-  when ".-."
-    output = "R"
-  when "..."
-    output = "S"
-  when "-"
-    output = "T"
-  when "..-"
-    output = "U"
-  when "...-"
-    output = "V"
-  when ".--"
-    output = "W"
-  when "-..-"
-    output = "X"
-  when "-.--"
-    output = "Y"
-  when "--.."
-    output = "Z"
-  else
-    output = ""
+MORSECODE = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_letter(input)
+  MORSECODE[input] || ''
+end
+
+def decode_word(word)
+  output = ''
+  array = word.split
+  array.each do |code|
+    output += decode_letter(code)
   end
-  return output
+  output
 end
 
-def decode_word (word)
-  output = ""
-  array = word.split(" ")
-  array.each {
-    |code| output += decode_letter (code)
-  }
-  return output
-end
-
-def decode_message (message)
-  output = ""
+def decode_message(message)
+  output = ''
   array = message.split('   ')
   array.each_with_index do |word, index|
-    output += decode_word (word)
-    if index != (array.length - 1) 
-      output += " "
-    end
+    output += decode_word(word)
+    output += ' ' if index != (array.length - 1)
   end
-  return output
+  output
 end
+
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
